a0865c3652d378f64ac41c345f7c2ec0d830fb7f
brysonprice <7bjprice@gmail.com>
2024-09-18 18:10:04 -0600

    began working on prep 2 activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string percent = Console.ReadLine();
+        int grade = int.Parse(percent);
+        if (grade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+
+        else if (80 <= grade)
+        {
+            Console.WriteLine("B");
+        }
     }
 }
