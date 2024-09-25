f9dde8ab2bb81cbecf446c6a39e328c3246afa20
brysonprice <7bjprice@gmail.com>
2024-09-25 10:04:00 -0600

    began prep4 activity

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = 1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string user_entry;
+            user_entry = Console.ReadLine();
+            number = int.Parse(user_entry);
+            numbers.Add(number);
+        }
+        foreach (int number in numbers)
+    // iterate through the list to adda  sum
     }
 }
