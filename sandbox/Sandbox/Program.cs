093c166b7953592e7698cc4cc28f5bb77d377123
brysonprice <7bjprice@gmail.com>
2024-09-25 13:58:24 -0600

    messed around with sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        static int AwesomeFunction(int x)
+        {
+            return 4 * x + 3;
+        }
+        Console.Write("Enter an integer: ");
+        string x_string = Console.ReadLine();
+        int x = int.Parse(x_string);
+        int y = AwesomeFunction(x);
+        Console.WriteLine(y);
     }
 }
