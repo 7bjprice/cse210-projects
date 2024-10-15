cc66c51a27b31dccac54fd93823cf0eabdf8fec4
brysonprice <7bjprice@gmail.com>
2024-10-15 17:27:25 -0600

    Completed Learning 03 activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Fraction one_one = new();
+        Fraction five_one = new(5);
+        Fraction three_four = new(3 ,4);
+        Fraction one_three = new(1, 3);
+         
+        Console.WriteLine(one_one.GetFractionString());
+        Console.WriteLine(one_one.GetTop());
+        Console.WriteLine(five_one.GetFractionString());
+        Console.WriteLine(five_one.GetTop());
+        Console.WriteLine(three_four.GetFractionString());
+        Console.WriteLine(three_four.GetDecimalValue());
+        Console.WriteLine(one_three.GetFractionString());
+        Console.WriteLine(one_three.GetDecimalValue());
     }
 }
